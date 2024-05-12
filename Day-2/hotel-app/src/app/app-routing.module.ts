0c966a7e0e9dfca0530bc7730079b747b4016054
AMMAD_IDREES\ammad <a.idrees@akili-innovation.com>
2024-05-12 15:05:23 +0200

    feat: add form validation in hotel app

--- Day-2/hotel-app/src/app/app-routing.module.ts
+++ Day-2/hotel-app/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationListComponent } from './reservation/reservation-list/reservation-list.component';
+import { ReservationFormComponent } from './reservation/reservation-form/reservation-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "reservation-list",
+    component: ReservationListComponent
+  },
+  {
+    path: "new-reservation",
+    component: ReservationFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
